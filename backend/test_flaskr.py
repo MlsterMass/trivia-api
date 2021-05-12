6f17c53fb241cb0e610e41fbb65be8d515dacd11
M1sterMass <alexmass1990@gmail.com>
2021-05-12 19:27:43 +0300

    update tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -65,8 +66,11 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertEqual(data['deleted'], 11)
 
-    def search_question(self):
-        res = self.client().post('questions/search', json={"searchTerm": "author"})
+    def test_search_question(self):
+        res = self.client().post(
+            'questions/search',
+            json={
+                "searchTerm": "author"})
         data = json.loads(res.data)
 
         self.assertEqual(data['success'], True)
@@ -74,12 +78,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
 
     def test_404_invalid_search_input(self):
-        res = self.client().post('questions/search', json={"searchTerm": "beer"})
+        res = self.client().post(
+            'questions/search',
+            json={
+                "searchTerm": "beer"})
         data = json.loads(res.data)
 
         self.assertEqual(data['success'], False)
         self.assertEqual(data['error'], 404)
-        self.assertEqual(data['message'], "Resource not found. Input out of range.")
+        self.assertEqual(
+            data['message'],
+            "Resource not found. Input out of range.")
 
     def test_create_question(self):
         new_question = {
